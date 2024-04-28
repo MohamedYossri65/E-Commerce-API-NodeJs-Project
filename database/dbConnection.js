05ebc966b64120aeb456f4bb0c0ff341ee08a3d9
mohamed yossri <mohamedyossri11@gmail.com>
2024-04-28 19:32:43 +0300

    deploy

--- database/dbConnection.js
+++ database/dbConnection.js
@@ -2,7 +2,7 @@ import  mongoose  from 'mongoose';
 
 export default function connectMongoDb()
 {
-    mongoose.connect(`mongodb+srv://yossri:yossri@cluster0.trb5jbe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`).then(()=>{
+    mongoose.connect(`mongodb+srv://yossri:yossri@cluster0.trb5jbe.mongodb.net/eCommmrceApi`).then(()=>{
         console.log('MongoDB Connection Succeeded.')
     }).catch((err) => {
             console.log('Error in DB connection: ' + err)
